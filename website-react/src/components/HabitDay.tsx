1f0b5bcce0a72b302c10b122af2f913630dd465c
fernandoprestes <fernandoprestesc@gmail.com>
2023-01-21 13:11:26 -0300

    feat: add checkbox component

--- website-react/src/components/HabitDay.tsx
+++ website-react/src/components/HabitDay.tsx
@@ -1,4 +1,5 @@
 import * as Popover from '@radix-ui/react-popover';
+import * as Checkbox from '@radix-ui/react-checkbox';
 import { ProgressBar } from './ProgressBar';
 import clsx from 'clsx';
 
@@ -29,6 +30,17 @@ export const HabitDay = function HabitDayComponent({ completed, amount }: HabitD
           <span className='mt-1 text-3xl font-extrabold leading-tight'>11/04</span>
 
           <ProgressBar progress={completedPercentage} />
+
+          <div className='mt-6 flex flex-col gap-3'>
+            <Checkbox.Root className=' flex items-center gap-3'>
+              <div className=' flex h-8 w-8 items-center justify-center rounded-lg border-2 border-zinc-800'>
+                <Checkbox.Indicator>
+                  <div className='i-ph-check h-5 w-5' />
+                </Checkbox.Indicator>
+              </div>
+              <span className='text-xl font-semibold leading-tight'>Beber água</span>
+            </Checkbox.Root>
+          </div>
 
           <Popover.Arrow
             height={8}
--- website-react/src/components/NewHabitForm.tsx
+++ website-react/src/components/NewHabitForm.tsx
@@ -1,6 +1,38 @@
-export const NewHabitForm = function NewHabitFormComponent() {
+import * as Checkbox from '@radix-ui/react-checkbox';
+import { FormEvent, useState } from 'react';
+
+const AVALIABLE_WEEK_DAYS = [
+  'Domingo',
+  'Segunda-feira',
+  'Terça-feira',
+  'Quarta-feira',
+  'Quinta-feira',
+  'Sexta-feira',
+  'Sabado',
+];
+
+export function NewHabitForm() {
+  const [title, setTitle] = useState('');
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  function createNewHabit(event: FormEvent) {
+    event.preventDefault();
+  }
+
+  function handleToggleWeekDay(weekDay: number) {
+    if (weekDays.includes(weekDay)) {
+      const weekDaysWithRemoved = weekDays.filter(day => day !== weekDay);
+      setWeekDays(weekDaysWithRemoved);
+    } else {
+      setWeekDays(prevState => [...prevState, weekDay]);
+    }
+  }
+
   return (
-    <form className='mt-6 flex w-full flex-col'>
+    <form
+      onSubmit={createNewHabit}
+      className='mt-6 flex w-full flex-col'
+    >
       <label
         htmlFor='title'
         className='font-semibold leading-tight'
@@ -13,6 +45,7 @@ export const NewHabitForm = function NewHabitFormComponent() {
         id='title'
         placeholder='Fazer alguma coisa, Fazer outra coisa, etc...'
         className='placeholder:(text-zinc-400) mt-3 rounded-lg bg-zinc-800 p-4 text-white'
+        onChange={event => setTitle(event.target.value)}
       />
 
       <label
@@ -21,6 +54,24 @@ export const NewHabitForm = function NewHabitFormComponent() {
       >
         Qual a recorrência?
       </label>
+
+      <div className='mt-3 flex flex-col gap-2'>
+        {AVALIABLE_WEEK_DAYS.map((weekDay, index) => (
+          <Checkbox.Root
+            key={weekDay}
+            className='flex items-center gap-3'
+            onCheckedChange={() => handleToggleWeekDay(index)}
+          >
+            <div className='flex h-8 w-8 items-center justify-center rounded-lg border-2 border-zinc-800'>
+              <Checkbox.Indicator>
+                <div className='i-ph-check h-5 w-5' />
+              </Checkbox.Indicator>
+            </div>
+            <span className='font-semibold leading-tight'>{weekDay}</span>
+          </Checkbox.Root>
+        ))}
+      </div>
+
       <button
         type='submit'
         className='hover:(bg-green-500) mt-6 flex items-center justify-center gap-3 rounded-lg bg-green-600 p-4 font-semibold'
@@ -30,4 +81,4 @@ export const NewHabitForm = function NewHabitFormComponent() {
       </button>
     </form>
   );
-};
+}
