41d710fed6aea9b7ae8c7a60a6d8c067379f885a
fernandoprestes <fernandoprestesc@gmail.com>
2023-01-22 15:12:51 -0300

    feat: add mobile animated

--- mobile/src/components/ProgressBar.tsx
+++ mobile/src/components/ProgressBar.tsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
 import { View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+  withDelay,
+} from "react-native-reanimated";
 
 interface ProgressBarProps {
   progress?: number;
@@ -7,12 +14,21 @@ interface ProgressBarProps {
 export const ProgressBar = function ProgressBarComponent({
   progress = 0,
 }: ProgressBarProps) {
+  const sharedProgress = useSharedValue(progress);
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: `${sharedProgress.value}%`,
+    };
+  });
+
+  useEffect(() => {
+    sharedProgress.value = withDelay(100, withTiming(progress));
+  }, [progress]);
+
   return (
     <View className="w-full h-3 rounded-xl bg-slate-700 mt-4">
-      <View
-        className="h-3 rounded-xl bg-violet-600"
-        style={{ width: `${progress}%` }}
-      />
+      <Animated.View className="h-3 rounded-xl bg-violet-600" style={style} />
     </View>
   );
 };
